1d1cf6179c95767b75c06ea607bc1183f32ba945
Cem Küçükhasanoğlu <gamedev.ckucukha@gmail.com>
2022-01-28 11:00:58 -0800

    game text display

---
+++ Assets/Scripts/Behavoiurs/GameTextDisplay.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Febucci.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTextDisplay : MonoBehaviour
+{
+    [NonSerialized] public Action Next;
+    
+    [SerializeField] private Button button;
+    [SerializeField] private TextAnimatorPlayer textPlayer;
+
+    private bool _isComplete;
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener(OnButtonClicked);
+        textPlayer.onTextShowed.AddListener(OnTextPlayerShowed);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(OnButtonClicked);
+        textPlayer.onTextShowed.RemoveListener(OnTextPlayerShowed);
+    }
+
+    public void DisplayText(string text)
+    {
+        textPlayer.ShowText(text);
+        textPlayer.StartShowingText();
+    }
+
+    private void OnButtonClicked()
+    {
+        if (_isComplete)
+        {
+            Next?.Invoke();
+            Debug.Log("Next");
+        }
+        else
+        {
+            textPlayer.SkipTypewriter();
+            Debug.Log("Skip typewriter");
+        }
+    }
+
+    private void OnTextPlayerShowed()
+    {
+        _isComplete = true;
+    }
+}
