12abcfe03acdc9168849bb80c00b1920d3befc7d
Cem Küçükhasanoğlu <gamedev.ckucukha@gmail.com>
2022-01-28 10:39:46 -0800

    decision id

--- Assets/Scripts/Models/Decision.cs
+++ Assets/Scripts/Models/Decision.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Decision
 {
+    public string id;
     public string text;
     public bool unlocked;
     public SerializableDictionary<Faction, int> outcomes;
