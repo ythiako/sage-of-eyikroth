34e984404246c123ed397c7b08ec450fd88f69e6
Cem Küçükhasanoğlu <gamedev.ckucukha@gmail.com>
2022-01-28 21:35:15 -0800

    singleton fix
    calling X.Instance inside OnDisable() causes new game object instance to be created. Thus, creating object instance is removed from singleton

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -3,30 +3,5 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T _instance;
-
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                // Search for existing instance.
-                _instance = (T)FindObjectOfType(typeof(T));
-
-                // Create new instance if one doesn't already exist.
-                if (_instance == null)
-                {
-                    // Need to create a new GameObject to attach the singleton to.
-                    var singletonObject = new GameObject();
-                    _instance = singletonObject.AddComponent<T>();
-                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
-
-                    // Make instance persistent.
-                    DontDestroyOnLoad(singletonObject);
-                }
-            }
-
-            return _instance;
-        }
-    }
+    public static T Instance => _instance ? _instance : _instance = FindObjectOfType<T>();
 }
